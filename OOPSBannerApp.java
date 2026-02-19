6b2d43aa5631d77727441b41b8a08c57862b2ae8
pramith111 <lpramith111@gmail.com>
2026-02-19 11:15:41 +0530

    Version 6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,61 @@
-//Author 5
-//Version 5
+//Author 6
+//Version 6
+
 public class OOPSBannerApp {
+
+    // Method to generate the pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    // Method to generate the pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Method to generate the pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[] {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
 
     public static void main(String[] args) {
 
-        String[] lines = {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-            String.join(" ", " *** ", " *** ", " ***** ", "***** "),
-            String.join(" ", "*   *", "*   *", "*   * ", " *     "),
-            String.join(" ", "*   *", "*   *", "***** ", " ***** "),
-            String.join(" ", "*   *", "*   *", "*     ", "     *"),
-            String.join(" ", " *** ", " *** ", "*     ", " ***** ")
-
-        };
-
-        for (String line : lines) {
-            System.out.println(line);
+        // Print O O P S
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
         }
     }
 }
